a03d19cddbb2777d3099ae02b1d3308907679786
IlyaKasiutin <boxfighterilya@gmail.com>
2023-05-27 01:47:47 +0300

    4 задача

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,31 +1,41 @@
+/*
+Написать алгоритм для решения игры в “пятнашки”. 
+Решением задачи является приведение к виду: [ 1 2 3 4 ] [ 5 6 7 8 ] [ 9 10 11 12] [ 13 14 15 0 ], 
+где 0 задает пустую ячейку. 
+Достаточно найти хотя бы какое-то решение. Число перемещений костяшек не обязано быть минимальным.
+*/
+
 #include <iostream>
 #include <array>
 #include <vector>
 #include <map>
-#include <queue>
+#include <set>
+#include <algorithm>
 
-static const size_t LINE_SIZE = 3;
+static const size_t LINE_SIZE = 4;
 static const size_t FIELD_SIZE = LINE_SIZE * LINE_SIZE;
 
 using FieldArray = std::array<uint8_t, FIELD_SIZE>;
 
 const FieldArray EMPTY_STATE = {
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
 };
 
 const FieldArray GOAL_STATE = {
-    1, 2, 3,
-    4, 5, 6,
-    7, 8, 9
-}; 
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 0
+};
 
 class FieldState
 {
 public:
-    FieldState(const FieldState& st) :state(st.state), zero_pos(st.zero_pos) {}
-    FieldState(const FieldArray& arr)
+    FieldState(const FieldState& st): state(st.state), zero_pos(st.zero_pos), distance(st.GetDistance()) {}
+    FieldState(const FieldArray& arr, long long distance = 0): distance(distance)
     {
         for (int i = 0; i < arr.size(); ++i)
         {
@@ -35,10 +45,12 @@ public:
         }
     }
 
+
     ~FieldState() {}
 
     std::vector<FieldState> GetNextVertices() const
     {
+        FieldState empty(EMPTY_STATE);
         std::vector<FieldState> result;
 
         if (zero_pos >= LINE_SIZE)
@@ -46,7 +58,9 @@ public:
             FieldState st(*this);
             st.move_up();
             result.push_back(st);
-        }
+        }    
+        else
+            result.push_back(empty);
 
         if (zero_pos < FIELD_SIZE - LINE_SIZE)
         {
@@ -54,6 +68,8 @@ public:
             st.move_down();
             result.push_back(st);
         }
+        else
+            result.push_back(empty);
 
         if (zero_pos % LINE_SIZE)
         {
@@ -61,6 +77,8 @@ public:
             st.move_left();
             result.push_back(st);
         }
+        else
+            result.push_back(empty);
 
         if ((zero_pos + 1) % LINE_SIZE)
         {
@@ -68,6 +86,7 @@ public:
             st.move_right();
             result.push_back(st);
         }
+        else result.push_back(empty);
         return result;
     }
 
@@ -99,6 +118,57 @@ public:
         std::cout << "_______________" << std::endl;
      }
 
+     bool HasSolution() const
+    {
+        int n = 0, k = 0;
+        k = zero_pos / LINE_SIZE + 1;
+
+        for (int i = 0; i < state.size(); ++i)
+        {
+            if (state[i] == 0)
+                continue;
+
+            for (int j = 0; j < i; ++j)
+            {
+                if (state[j] > state[i] && state[j] != 0)
+                {
+                    n++;
+                }
+            }
+        }
+        return (n + k) % 2 == 0;
+    }
+
+    void SetDistance(long long dist)
+    {
+        distance = dist;
+    }
+
+    long long GetDistance() const
+    {
+        return distance;
+    }
+
+    long long GetMetric() const
+    {
+        double coeff = 3.0;
+
+        if (state == EMPTY_STATE)
+            return LINE_SIZE * LINE_SIZE * FIELD_SIZE + distance;
+
+        int metric = 0;
+
+        for (int i = 0; i < FIELD_SIZE; ++i)
+        {
+            if (state[i] == 0)
+                continue;
+            metric += (int) (manhattan_distance(state[i], i % LINE_SIZE, i / LINE_SIZE));
+        }
+        return coeff * metric + distance;
+    }
+
+
+
 private:
     void move_up()
     {
@@ -125,69 +195,125 @@ private:
         zero_pos = new_zero_pos;
     }
 
+    int get_x(uint8_t value) const
+    {
+        if (value == 0)
+            return LINE_SIZE - 1;
+        return ((int)value - 1) % LINE_SIZE;
+    }
+
+    int get_y(uint8_t value) const
+    {
+        if (value == 0)
+            return LINE_SIZE - 1;
+        return ((int)value - 1) / LINE_SIZE;
+    }
+
+    int manhattan_distance(uint8_t value, int cur_x, int cur_y) const
+    {
+        int right_x = get_x(value);
+        int right_y = get_y(value);
+        return std::abs(cur_x - right_x) + std::abs(cur_y - right_y);
+    }
+
+
     FieldArray state;
     int zero_pos;
+    long long distance;
 };
 
-void bfs(FieldState start)
+void a_star(FieldState start)
 {
+    if (!start.HasSolution())
+    {
+        std::cout << -1 << std::endl;
+        return;
+    }
+
     FieldState empty(EMPTY_STATE);
     FieldState goal(GOAL_STATE);
 
     std::map<FieldState, FieldState> visited;
     visited.insert({start, empty});
 
-    std::queue<FieldState> q;
-    q.push(start);
+    auto cmp = [](FieldState left, FieldState right)
+    {
+        if (left.GetMetric() < right.GetMetric())
+        {
+            return true;
+        }
+        return false;
+    };
+    std::multiset<FieldState, decltype(cmp) > q(cmp);
+    q.insert(start);
 
     FieldState current(EMPTY_STATE);
     while (!q.empty())
     {
-        current = q.front();
-        q.pop();
+        current = *q.begin();
+        q.erase(q.begin());
 
         if (current == goal)
         {
             break;
         }
-
+        
         std::vector<FieldState> children = current.GetNextVertices();
         for (auto it: children)
         {
+            it.SetDistance(current.GetDistance() + 1);
+
             if (visited.find(it) == visited.end())
             {
                 visited.insert({it, current});
-                q.push(it);
-            }
+                q.insert(it);
+            }            
         }
     }
 
-    if (q.empty())
+    int steps_count = 0;
+    std::string path;
+    while (current != empty)
     {
-        std::cout << -1 << std::endl;
-    }
-    else
-    {
-        int steps_count = 0;
-        while (current != empty)
+        FieldState previous = visited.find(current)->second;
+        if (previous == empty)
+            break;
+        std::vector<FieldState> children = previous.GetNextVertices();
+        if (current == children[0])
         {
-            current.print();
-            current = visited.find(current)->second;
-            steps_count++;
+            path += "D";
         }
-        std::cout << steps_count << std::endl;
+        else if (current == children[1])
+        {
+            path += "U";
+        }
+        else if (current == children[2])
+        {
+            path += "R";
+        }
+        else if (current == children[3])
+        {
+            path += "L";
+        }
+
+        current = visited.find(current)->second;
+        steps_count++;
     }
+    std::cout << steps_count << std::endl;
+    std::reverse(path.begin(), path.end());
+    std::cout << path << std::endl;
 }
 
 int main()
 {
-    FieldState ex(
-        {
-            1, 2, 3,
-            4, 5, 0,
-            7, 8, 6
-        }
-    );
-    bfs(ex);
+    FieldArray input;
+    for (int i = 0; i < FIELD_SIZE; ++i)
+    {
+        int elem;
+        std::cin >> elem;
+        input[i] = elem;
+    }
+    FieldState ex(input);
+    a_star(ex);
     return 0;
 }
